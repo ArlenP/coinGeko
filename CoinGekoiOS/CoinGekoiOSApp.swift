b67dc157f4f00e143b9ba65fc148318d83321800
Arlen Peña <apena@hypertech.mx>
2024-04-19 17:04:37 -0600

    Initial Commit

---
+++ CoinGekoiOS/CoinGekoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoiOSApp.swift
+//  CoinGekoiOS
+//
+//  Created by Arlen Peña on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
